efa068f38ee0028b579cbecfabd44981c5ab2442
Varun Verma <varun7086verma@gmail.com>
2025-06-08 20:44:40 +0530

    customization done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router"
+import Homepage from "./pages/Homepage"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Blogs from "./pages/Blogs"
+import Contact from "./pages/Contact"
+
 
 const App = () => {
+
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../features/Navbar/Navbar"
+
+const Homepage = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Homepage
